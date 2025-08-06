ff2578e3329861754a911086e9ec2bf480a781b9
Wolfchamane <arturo.martinez@amartinez.dev>
2025-08-06 12:43:53 +0200

    feature (*): load data from static source using fetch

--- landing/src/script.js
+++ landing/src/script.js
@@ -1,3 +1,86 @@
+const _loadData = async () => {
+    let url = window.location.href;
+    if (url.endsWith('/')) {
+        url = url.substring(0, Number(url.length - 1));
+    }
+    url = [url, 'data.json'].join('/');
+    const res = await fetch(new URL(url), {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    return await res.json();
+};
+
+const otherQueriesWATextTemplate = ({ name = '', text = '' }) => `Hola, soy ${name}.
+
+${text}
+
+Muchas gracias`;
+
+const OTHER_QUERIES_FORM_INPUTS = {
+    name: {
+        required: true,
+        kind: 'input',
+        type: 'text',
+        placeholder: 'Tú nombre'
+    },
+    'other-questions': {
+        required: true,
+        kind: 'textarea',
+        placeholder: 'Escribe aquí tú mensaje ...',
+        rows: 4
+    }
+};
+
+const CONTACT_FORM_INPUTS = DATA => ({
+    service: {
+        required: true,
+        kind: 'select',
+        type: '',
+        options: [{ value: '', title: '-- Elige un servicio --' }, ...DATA.services]
+    },
+    date: {
+        required: true,
+        kind: 'input',
+        type: 'date'
+    },
+    name: {
+        required: true,
+        kind: 'input',
+        type: 'text',
+        placeholder: 'Tú nombre'
+    },
+    email: {
+        required: true,
+        kind: 'input',
+        type: 'email',
+        placeholder: 'Tú email de contacto'
+    },
+    phone: {
+        required: true,
+        kind: 'input',
+        type: 'tel',
+        placeholder: 'Tú teléfono de contacto'
+    },
+    'accept-checkbox': {
+        required: true,
+        kind: 'input',
+        type: 'checkbox',
+        className: 'checkbox-wrapper',
+        children: parent => {
+            const link = document.createElement('A');
+            link.setAttribute('href', './politica-de-privacidad.html');
+            link.innerText = 'política de protección de datos';
+            const text = document.createTextNode('He léido y acepto la ');
+            const span = document.createElement('SPAN');
+            span.appendChild(text);
+            span.appendChild(link);
+            parent.appendChild(span);
+        }
+    }
+});
+
 const _setBusinessData = (map = []) => {
     map.forEach(({ query = '', attributes = {}, innerText = '', children = null }) => {
         const node = document.querySelector(query);
@@ -16,41 +99,95 @@ const _setBusinessData = (map = []) => {
     });
 };
 
-document.addEventListener('DOMContentLoaded', function () {
-    const data = Object.freeze({
-        phoneNumber: '34667798497',
-        email: 'centralboxdetailing@gmail.com',
-        address: 'Avenida del General Perón, 21, Parking Presidente Carmona Planta -1, 28020 Madrid',
-        facebook: 'https://www.facebook.com/Centralbox-Detailing-Bernabéu-107181584752801/',
-        instagram: 'https://www.instagram.com/centralboxdetailing',
-        mapsUrl: 'https://maps.app.goo.gl/8kWw15X3ArqySUiy7',
-        mapsIframeUrl:
-            'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6072.06044302646!2d-3.6982179237255925!3d40.452468071434005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4229d4bd3a32ef%3A0xa125bb7dfd05c972!2sCentralbox%20Detailing%20Bernab%C3%A9u!5e0!3m2!1ses!2ses!4v1753920211398!5m2!1ses!2ses"'
+const _bindContactFormCheckboxWithButton = ({ checkbox, button }) => {
+    if (checkbox && button) {
+        checkbox.addEventListener('change', () => {
+            const isChecked = checkbox.checked;
+            if (isChecked) {
+                button.removeAttribute('disabled');
+            } else {
+                button.setAttribute('disabled', 'disabled');
+            }
+        });
+    }
+};
+
+const _injectForm = ({ parent, props = {}, inputs = [], submitButton }) => {
+    const form = document.createElement('FORM');
+    Object.keys(props).forEach(prop => form.setAttribute(prop, props[prop]));
+    form.style.marginTop = '1em';
+
+    Object.keys(inputs).forEach(key => {
+        const { required, kind, type, options, placeholder, children, className, ...rest } = inputs[key];
+        const label = document.createElement('LABEL');
+        label.setAttribute('for', key);
+        const input = document.createElement(kind);
+        input.setAttribute('id', key);
+        input.setAttribute('required', `${!!required}`);
+        Object.keys(rest).forEach(attr => input.setAttribute(attr, rest[attr]));
+
+        switch (kind) {
+            case 'select':
+                (options || []).forEach(({ value, title }) => {
+                    const option = document.createElement('OPTION');
+                    option.setAttribute('value', value);
+                    option.innerText = title;
+                    input.appendChild(option);
+                });
+                break;
+            case 'input':
+            default:
+                input.setAttribute('name', key);
+                input.setAttribute('type', type || 'text');
+                input.setAttribute('placeholder', placeholder || '');
+                break;
+        }
+
+        label.appendChild(input);
+
+        if (className) {
+            label.classList.add(className);
+        }
+
+        if (children instanceof Function) {
+            children(label);
+        }
+
+        form.appendChild(label);
     });
 
-    const WHASTAPP_LINK = `https://wa.me/${data.phoneNumber}`;
+    if (submitButton instanceof Function) {
+        submitButton(form);
+    }
+
+    parent.appendChild(form);
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const DATA = await _loadData();
+    const WHATSAPP_LINK = `https://wa.me/${DATA.phoneNumber}`;
 
     _setBusinessData([
         {
             query: '[data-ref="phone"]',
-            attributes: { href: `tel:+${data.phoneNumber}` },
-            innerText: data.phoneNumber.replace(/(\d{2})(\d{3})(\d{3})(\d{3})/, '(+$1) $2 $3 $4')
+            attributes: { href: `tel:+${DATA.phoneNumber}` },
+            innerText: DATA.phoneNumber.replace(/(\d{2})(\d{3})(\d{3})(\d{3})/, '(+$1) $2 $3 $4')
         },
         {
             query: '[data-ref="whatsapp"]',
-            attributes: { href: WHASTAPP_LINK }
+            attributes: { href: WHATSAPP_LINK }
         },
         {
             query: '[data-ref="email"]',
-            attributes: { href: `mailto:${data.email}` },
-            innerText: `${data.email}`
+            attributes: { href: `mailto:${DATA.email}` },
+            innerText: `${DATA.email}`
         },
         {
             query: '[data-ref="address"]',
             children: () => {
                 const link = document.createElement('a');
-                const text = document.createTextNode(data.address);
-                link.setAttribute('href', data.mapsUrl);
+                const text = document.createTextNode(DATA.address);
+                link.setAttribute('href', DATA.mapsUrl);
                 link.setAttribute('target', '_blank');
                 link.setAttribute('aria-label', 'Goggle Maps');
                 link.append(text);
@@ -60,17 +197,76 @@ document.addEventListener('DOMContentLoaded', function () {
         },
         {
             query: '[data-ref="facebook"]',
-            attributes: { href: data.facebook }
+            attributes: { href: DATA.facebook }
         },
         {
             query: '[data-ref="instagram"]',
-            attributes: { href: data.instagram }
+            attributes: { href: DATA.instagram }
         },
         {
             query: '[data-ref="maps-iframe"]',
-            attributes: { src: data.mapsIframeUrl }
+            attributes: { src: DATA.mapsIframeUrl }
         }
     ]);
+
+    _injectForm({
+        parent: document.querySelector('#contacto #contact-form-wrapper'),
+        props: {
+            id: 'booking-form',
+            href: `https://formsubmit.co/${encodeURIComponent(DATA.email)}`,
+            method: 'POST'
+        },
+        inputs: CONTACT_FORM_INPUTS(DATA),
+        submitButton: form => {
+            const button = document.createElement('BUTTON');
+            button.setAttribute('id', `${form.getAttribute('id')}-submit`);
+            button.setAttribute('type', 'submit');
+            button.setAttribute('disabled', 'disabled');
+            button.classList.add('cta-button');
+            button.innerText = 'Solicitar Reserva';
+            form.appendChild(button);
+        }
+    });
+    _bindContactFormCheckboxWithButton({
+        checkbox: document.querySelector('#accept-checkbox'),
+        button: document.querySelector(`#booking-form-submit`)
+    });
+    _injectForm({
+        parent: document.querySelector('#contacto #other-queries'),
+        props: {
+            id: 'booking-form',
+            href: `https://formsubmit.co/${encodeURIComponent(DATA.email)}`,
+            method: 'POST'
+        },
+        inputs: OTHER_QUERIES_FORM_INPUTS,
+        submitButton: form => {
+            const div = document.createElement('DIV');
+            div.classList.add('form-buttons');
+            const submitButton = document.createElement('BUTTON');
+            submitButton.setAttribute('type', 'submit');
+            submitButton.classList.add('cta-button-secondary');
+            submitButton.innerText = 'Enviar consulta por email';
+            div.appendChild(submitButton);
+            const waButton = document.createElement('BUTTON');
+            waButton.classList.add('cta-button-whatsapp');
+            waButton.addEventListener('click', () => {
+                const userMessage = otherQueriesWATextTemplate({
+                    name: form.name.value,
+                    text: form['other-questions'].value
+                });
+                const whatsappUrl = `${WHATSAPP_LINK}?text=${encodeURIComponent(userMessage)}`;
+                window.open(whatsappUrl, '_blank');
+            });
+            const waIcon = document.createElement('I');
+            waIcon.classList.add('fab');
+            waIcon.classList.add('fa-whatsapp');
+            const waText = document.createTextNode(' Enviar por WhatsApp');
+            waButton.appendChild(waIcon);
+            waButton.appendChild(waText);
+            div.appendChild(waButton);
+            form.appendChild(div);
+        }
+    });
 
     // --- NAVEGACIÓN MÓVIL ---
     const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
@@ -111,46 +307,4 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
-
-    // --- BOTÓN DE WHATSAPP ---
-    const whatsappButton = document.getElementById('whatsapp-button');
-    const contactFormMessage = document.querySelector('#contact-form textarea[name="message"]');
-
-    whatsappButton.addEventListener('click', () => {
-        const userMessage = contactFormMessage.value;
-        const whatsappUrl = `${WHASTAPP_LINK}?text=${encodeURIComponent(userMessage)}`;
-        window.open(whatsappUrl, '_blank');
-    });
-
-    // --- MANEJO DE FORMULARIOS (PREVENCIÓN) ---
-    // Muestra una alerta en lugar de enviar el formulario, ya que no hay backend
-    const bookingForm = document.getElementById('booking-form');
-    const emailContactForm = document.getElementById('contact-form');
-
-    bookingForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-        alert(
-            'Gracias por tu solicitud de reserva. Nos pondremos en contacto contigo pronto para confirmar los detalles.'
-        );
-        bookingForm.reset();
-    });
-
-    emailContactForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-        alert('Gracias por tu mensaje. Te responderemos lo antes posible.');
-        emailContactForm.reset();
-    });
-
-    const acceptCheckbox = document.querySelector('#accept-checkbox');
-    const formSubmitButton = document.querySelector('#form-submit-button');
-    if (acceptCheckbox && formSubmitButton) {
-        acceptCheckbox.addEventListener('change', () => {
-            const isChecked = acceptCheckbox.checked;
-            if (isChecked) {
-                formSubmitButton.removeAttribute('disabled');
-            } else {
-                formSubmitButton.setAttribute('disabled', 'disabled');
-            }
-        });
-    }
 });
